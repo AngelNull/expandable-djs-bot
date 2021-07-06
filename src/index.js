601bdb38f32b4d9697a8e5ec733b7aa309ab3bc3
Angel <69059762+AngelNull@users.noreply.github.com>
2021-07-07 00:46:57 +0100

    feat(index): log handler on load

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const { client, translate: trans, lang } = require('./client/core.js');
+const chalk = require('chalk');
+var recursive = require('recursive-readdir');
 
 fs.readdir('./src/events/', (err, files) => {
     if (err) return console.error(err);
     files.forEach((file) => {
         if (!file.endsWith('.js')) return;
         const event = require(`./events/${file}`);
-        console.log(trans('LOAD_EVENT', lang(), file));
+        console.log(`${chalk.yellow(`${trans('LOAD_EVENT', lang(), file)}`)}`);
         let eventName = file.split('.')[0];
         client.on(eventName, event.bind(null, client));
         delete require.cache[require.resolve(`./events/${file}`)];
@@ -19,15 +21,15 @@ fs.readdir('./src/commands/', (err, files) => {
         if (!file.endsWith('.js')) return;
         let props = require(`./commands/${file}`);
         let commandName = file.split('.')[0];
-        console.log(trans('LOAD_COMMAND', lang(), file));
+        console.log(`${chalk.yellow(`${trans('LOAD_COMMAND', lang(), file)}`)}`);
         client.commands.set(commandName, props);
     });
 });
 
-fs.readdir('./src/handlers/', (err, files) => {
+recursive('./src/handlers/', (err, files) => {
     if (err) return console.error(err);
     files.forEach((file) => {
-        if (!file.endsWith('.js') || file == 'index.js') return;
-        console.log(trans('LOAD_HANDLER', lang(), file));
+        if (!file.endsWith('.js') || file == 'src\\handlers\\index.js') return;
+        console.log(`${chalk.yellow(`${trans('LOAD_HANDLER', lang(), file)}`)}`);
     });
 });
