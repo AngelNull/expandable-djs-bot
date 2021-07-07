2942e34148ec68e068a16bc966ab007662d9b391
Angel <69059762+AngelNull@users.noreply.github.com>
2021-07-07 19:08:49 +0100

    feat(ready): deploy privately on startup

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,13 +2,20 @@ const { translate: trans, lang } = require('../handlers/language/translator');
 const chalk = require('chalk');
 
 require('dotenv').config();
-module.exports = (client) => {
-    /* Omitted once the bot has successfully authenticated */
+module.exports = async (client) => {
+    /* Authenticated */
     console.log(`${chalk.green(`${trans('CLIENT_READY', lang(), client.user.tag, new Date())}`)}`);
     /* Set the custom presence and activity for the bot */
+    setPresence(client);
+    /* Deploy commands in the test server on launch */
+    await client.guilds.cache.get(process.env.guildID)?.commands.set(client.commands);
+    return console.log(trans('REDEPLOY_PRIVATE_DONE', lang()));
+};
+
+function setPresence(client) {
     if (process.env.enableCustomActivity && process.env.streamingURL != 'None')
         client.user.setPresence({ activities: [{ name: process.env.botActivity, type: process.env.botActivityType, url: process.env.streamingURL }], status: process.env.botStatus });
     else if (process.env.enableCustomActivity && process.env.streamingURL == 'None')
         client.user.setPresence({ activities: [{ name: process.env.botActivity, type: process.env.botActivityType }], status: process.env.botStatus });
     else client.user.setPresence({ status: process.env.botStatus });
-};
+}
